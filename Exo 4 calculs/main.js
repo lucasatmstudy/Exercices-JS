2822650f3ddbc06d02f0b41b6925fa9a59bebfc7
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-11 16:24:11 +0200

    Exercice 4 Calcul

---
+++ Exo 4 calculs/main.js
@@ -0,0 +1,29 @@
+let laAddition = 25.4+24.6
+let laDivision = 100/20
+let laMultiplication = 25*5
+let laSoustraction = 1000-990
+console.log(laMultiplication,laAddition,laDivision,laSoustraction)
+console.log((laMultiplication-(laAddition+laDivision))/laSoustraction)
+const leTotal = (laMultiplication-(laAddition+laDivision))/laSoustraction
+console.log(leTotal)
+
+let unCompteur = 1
+unCompteur ++
+unCompteur --
+unCompteur = unCompteur*10
+unCompteur = unCompteur/2
+console.log(unCompteur)
+
+function leCompteur(nombreBase) {
+    return nombreBase+unCompteur
+}
+
+console.log(leCompteur(leTotal))
+
+console.log("Puissance du leTotal", leTotal,"par 3", "est égale à", Math.pow(leTotal, 3))
+
+console.log("Suite de l'exercice,2^10")
+let chiffreBase = 2
+let laPuissance = 10
+
+console.log(Math.pow(chiffreBase, laPuissance))
