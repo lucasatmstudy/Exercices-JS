3bce3da6aca5f5a57d49e9856bbf169474ff7c7e
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-12 11:42:19 +0200

    Pizza

---
+++ Exo-vite-et-daisyUI/src/exercices/Exo1JS/mainexo1.js
@@ -0,0 +1,5 @@
+console.log("HELLO TEAM");
+
+// Je veux changer la couleur du titre en rouge
+let titrepage = document.querySelector("h1")
+titrepage.style.color = "chartreuse";
---
+++ Exo-vite-et-daisyUI/src/exercices/Exo5Pizza/pizza.js
@@ -0,0 +1,21 @@
+let prenom = "Marie";
+let age = 30;
+let ville = "Paris";
+let blague = 'Qu\'est-ce qu\'une pizza a dit à une autre pizza qui lui demandait des conseils? \"Suis ta pâte et tout ira bien!\"'
+let pizza = '"Rafinata"'
+
+let commande = {
+    article: 2,
+    pizza: "calzone",
+    prix: 12.5 + 12.5,
+    adresse: "11 avenue de l'europe",
+    temps: "30",
+};
+
+let SumUpOrderPhrase = `Bonjour ${prenom}, merci pour votre commande : ${commande.pizza} est en cours de préparation.
+Elle vous sera livrée d'ici ${commande.temps} minutes à votre adresse ${commande.adresse}.
+Prix total :${commande.prix} euros.
+Merci d'avoir commandé chez la pizzerria "Rafinata".
+${blague}`;
+
+console.log(SumUpOrderPhrase)
