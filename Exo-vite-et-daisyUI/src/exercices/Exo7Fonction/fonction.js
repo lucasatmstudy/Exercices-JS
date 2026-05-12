84d562a269571b64e4dc4ad0017184e06287ab92
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-12 16:06:34 +0200

    fonction

---
+++ Exo-vite-et-daisyUI/src/exercices/Exo7Fonction/fonction.js
@@ -0,0 +1,11 @@
+unNombre()
+deuxNombre(100)
+
+function unNombre(nombreA=0) {
+    console.log(nombreA+33);
+}
+
+function deuxNombre(nombreA=0, nombreB=0) {
+    console.log(nombreA+nombreB);
+}
+
