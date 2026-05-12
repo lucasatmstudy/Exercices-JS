6797d8b6ff10849cde59839765fbfea2f9ad5d5d
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-12 12:06:29 +0200

    Arrays

---
+++ Exo-vite-et-daisyUI/src/exercices/Exo6Arrays/Arrays.js
@@ -0,0 +1,8 @@
+let prenom = "José";
+let age = 54;
+let passion = ["La chasse", "WWF"]
+let tabUser = [prenom, age, passion]
+
+console.log(tabUser)
+console.log(tabUser[2])
+console.log(tabUser[2][1])
