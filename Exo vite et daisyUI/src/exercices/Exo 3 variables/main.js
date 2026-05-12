5ce5703ab0fd9a69be62f3854688801e10b3a43d
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-12 09:25:52 +0200

    Regrouper exo pour saisyUI

---
+++ Exo vite et daisyUI/src/exercices/Exo 3 variables/main.js
@@ -0,0 +1,11 @@
+let string = "Les vairables";
+let number = 25;
+let numberVirgule = 12.5;
+let classe = ["naissance", "vie", "mort"];
+let quidonc = {
+    nom: "Marc",
+    travail: "Prof",
+    age: "35",
+};
+console.log(string, number, numberVirgule, classe, quidonc);
+console.log(classe[1],quidonc.nom)
