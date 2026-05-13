fd99f5c03b77d8ecb1b4e1dd9bda5d40a68c4c43
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-13 12:23:09 +0200

    Tibo if else

--- Exo-vite-et-daisyUI/src/exercices/Exo8/boucleTibo.js
+++ Exo-vite-et-daisyUI/src/exercices/Exo8/boucleTibo.js
@@ -1,12 +1,30 @@
 let noteTibo = [0,3,4,6,18,19,4];
 
-function calculerMoyenne(tableau) {
+// function calculerMoyenne(tableau) {
+//     let sum = 0;
+//     for (let i = 0; i < tableau.length; i++) {
+//         sum = sum + tableau[i];
+//     }
+//     return sum / tableau.length;
+// }
+
+// console.log(noteTibo)
+// console.log(calculerMoyenne(noteTibo))
+
+function calculerMoyenneMention(tableau) {
     let sum = 0;
-    for (i = 0; i < tableau.length; i++) {
-        sum = sum + tableau[i]
+    for (let i = 0; i < tableau.length; i++) {
+        sum += tableau[i];
     }
-    return sum / tableau.length
+    moyenne = sum / tableau.length;
+
+    if (moyenne >= 15)
+        mention = "Très bien"
+    else if (moyenne >= 10)
+        mention = "Assez bien"
+    else 
+        mention = "Refus"
+    return "La moyenne est :" + moyenne.toFixed(2) + mention
 }
 
-console.log(noteTibo)
-console.log(calculerMoyenne(noteTibo))
+console.log(calculerMoyenneMention(noteTibo))
